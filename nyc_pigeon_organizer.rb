bf3b4aab5fa411b6251bd73c24cf7467b53f8b8f
Robert Ng <robertng85@gmail.com>
2016-06-13 14:33:32 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+   data.each do |attribute, attribute_hash|
+   attribute_hash.each do |specific_attribute, pigeon_arr|
+   pigeon_arr.each do |pigeon|
+   if pigeon_list[pigeon].nil?
+   pigeon_list[pigeon] = {}
+   end
+
+pigeon_list[pigeon][attribute] ||= []
+pigeon_list[pigeon][attribute] << specific_attribute.to_s
+end
+end
+end
+pigeon_list
 end
